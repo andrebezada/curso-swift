bb48affe569160605e62c9d4dfcfb0d141335e3d
andrebezada <a.bezada.maza@gmail.com>
2021-07-29 23:31:52 -0500

    tipos de asignacion a memoria ram

---
+++ 02_var_let.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+let maximumNumberOfLoginAttempts = 3
+
+var currentLoginAttempt = 0
+
+var x = 0.0, y = 0.0, z = 0.0
+
+
+var welcomeMessage : String
+
+welcomeMessage = "Hola, ¿que tal?"
+
+var red, gree, blue : Double
+
+
+let π = 3.141592
+
+var 👽 = "Alien"
+
+print(👽)
+
+👽 = "nuevo nombre"
+
+print("el numero de login actual es: \(currentLoginAttempt) de un total disponible de \(maximumNumberOfLoginAttempts)")
+
+//Esto es un comentario
+
+/* Esto es un comentario
+    ocupa varias lineas
+ 
+ Y puede escribar muchas cosas */
+
+/* esto es un comentario
+ /*Este es un comentario aninado */
+ Esto es otro comentario multilinea*/
---
+++ 03_numbers.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+let age : UInt8 = 31
+
+let minValue = UInt8.min
+let maxValue = UInt8.max
+
+
+let f1 : Float = 3.14159265 //hasta 6 decimales
+let d1: Double = 3.14159265 //hasta 12 decimales o mas
+
+let meaningOfLife = 42 //Int
+let pi = 3.14159 // Double
+let anotherPi = 3 + 0.14159 //Double
+
+let decimalInteger = 17 // 1*10 + 7*10^0
+let binaryInteger = 0b10001 // 1*2^4 + 0*2^3 ....
+let octaInteger = 0o21 // 2*8^1 + 1*8^0
+let hexadecimalInteger = 0x11 // 1*16^1 + 1*16^0
+
+let decimalDouble = 12.1875
+let exponentDouble = 1.21875e1
+let hexadecimalDouble = 0xC.3p0
+
+let paddedDouble = 000123.456
+let someNumber = 00000097.540
+
+let oneMillion = 1_000_000
+let justMoreThanAMillion = 1_000_000.000_000_1
+
+//let cannotBeNegative : UInt8 = -1
+//let tooBig : UInt8 = UInt8.max + 1
